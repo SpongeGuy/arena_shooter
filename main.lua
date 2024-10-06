6288d6445809c2ca97207853150255cc2f1dfca7
Sponge Guy <realevan03@gmail.com>
2024-10-05 21:49:02 -0400

    polygon rotation, also refactor `shape` to `polygon`

--- main.lua
+++ main.lua
@@ -65,32 +65,46 @@ function player:shoot()
 end
 
 local pentagron = {
-	vertices = {520, 520, 480, 520, 480, 460,},
-	rotation_speed = 1, -- rotation at radians per frame
-	center_x = 500,
-	center_y = 500,
+	vertices = {520, 520, 480, 520, 480, 460, 800, 600},
+	rotation_speed = 0.25, -- rotation at radians per frame
+	center_x = 551,
+	center_y = 510,
 }
 
-function vector_rotate(x, y, cx, cy, angle)
+function polygon_rotate(dt, polygon)
+	local angle = polygon.rotation_speed * dt
 	local s = math.sin(angle)
 	local c = math.cos(angle)
 
-	x = x - cx
-	y = y - cy
+	-- this is pain
+	for i = 1, #polygon.vertices, 2 do
+		local x = polygon.vertices[i] - polygon.center_x
+		local y = polygon.vertices[i+1] - polygon.center_y
 
-	local xnew = x * c - y * s
-	local ynew = x * s + y * c
+		local xnew = x * c - y * s
+		local ynew = x * s + y * c
 
-	x = xnew + cx
-	y = ynew + cy
-	return x, y
+		x = xnew + polygon.center_x
+		y = ynew + polygon.center_y
+		polygon.vertices[i], polygon.vertices[i+1] = x, y
+	end
 end
 
-function transform(dt, shape)
-	local angle = shape.rotation_speed * dt
-	for i = 1, #shape.vertices, 2 do
-		shape.vertices[i], shape.vertices[i+1] = vector_rotate(shape.vertices[i], shape.vertices[i+1], shape.center_x, shape.center_y, angle)
+function polygon_translate(dt, polygon, vector)
+	local dx = vector[1] * dt
+	local dy = vector[2] * dt
+	for i = 1, #polygon.vertices, 2 do
+		
+		local x = polygon.vertices[i]
+		local y = polygon.vertices[i+1]
+
+		x = x + dx
+		y = y + dy
+
+		polygon.vertices[i], polygon.vertices[i+1] = x, y
 	end
+	polygon.center_x = polygon.center_x + dx
+	polygon.center_y = polygon.center_y + dy
 end
 
 function offset(vector, offset)
@@ -122,7 +136,9 @@ function love.update(dt)
 		player.shot_timer = love.timer.getTime()
 	end
 
-	transform(dt, pentagron)
+	polygon_translate(dt, pentagron, {100, -70})
+	polygon_rotate(dt, pentagron)
+
 end
 
 function love.draw()
@@ -132,4 +148,6 @@ function love.draw()
 		love.graphics.circle('fill', b.coordinates[1], b.coordinates[2], b.size)
 	end
 	love.graphics.polygon('fill', pentagron.vertices)
+	love.graphics.print(love.mouse.getX(), love.mouse.getX() + 30, love.mouse.getY() + 30)
+	love.graphics.print(love.mouse.getY(), love.mouse.getX() + 60, love.mouse.getY() + 30)
 end
