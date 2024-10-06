e516b28240e26095e9ae962ef91d3db0293a803d
Sponge Guy <realevan03@gmail.com>
2024-10-06 00:48:33 -0400

    bounding box and polygon.lua

---
+++ lib/polygon.lua
@@ -0,0 +1,81 @@
+local polygon = {}
+
+function polygon:get_bounding_box(polygon)
+	local x_max = 0
+	local y_max = 0
+	local x_min = 999999
+	local y_min = 999999
+	for i = 1, #polygon.vertices, 2 do
+		if polygon.vertices[i] > x_max then x_max = polygon.vertices[i] end
+		if polygon.vertices[i] < x_min then x_min = polygon.vertices[i] end
+		if polygon.vertices[i+1] > y_max then y_max = polygon.vertices[i+1] end
+		if polygon.vertices[i+1] < y_min then y_min = polygon.vertices[i+1] end
+	end
+	print(x_min, y_min)
+	return {x_min, y_min, x_max, y_max}
+end
+
+
+
+function polygon:get_centroid(polygon)
+	local sum_x = 0
+	local sum_y = 0
+	for i = 1, #polygon.vertices, 2 do
+		sum_x = sum_x + polygon.vertices[i]
+		sum_y = sum_y + polygon.vertices[i+1]
+	end
+
+	local cx = sum_x / (#polygon.vertices / 2)
+	local cy = sum_y / (#polygon.vertices / 2)
+
+	return {cx, cy}
+end
+
+
+function polygon:rotate(dt, polygon)
+	local angle = polygon.rotation_speed * dt
+	local s = math.sin(angle)
+	local c = math.cos(angle)
+
+	-- this is pain
+	for i = 1, #polygon.vertices, 2 do
+		local x = polygon.vertices[i] - polygon.center[1]
+		local y = polygon.vertices[i+1] - polygon.center[2]
+
+		local xnew = x * c - y * s
+		local ynew = x * s + y * c
+
+		x = xnew + polygon.center[1]
+		y = ynew + polygon.center[2]
+		polygon.vertices[i], polygon.vertices[i+1] = x, y
+	end
+
+end
+
+function polygon:translate(dt, polygon, vector)
+	local dx = vector[1] * dt
+	local dy = vector[2] * dt
+	for i = 1, #polygon.vertices, 2 do
+		
+		local x = polygon.vertices[i]
+		local y = polygon.vertices[i+1]
+
+		x = x + dx
+		y = y + dy
+
+		polygon.vertices[i], polygon.vertices[i+1] = x, y
+	end
+	polygon.center[1] = polygon.center[1] + dx
+	polygon.center[2] = polygon.center[2] + dy
+end
+
+function polygon:create(vertices, rotation_speed)
+	local p = {}
+	p.vertices = vertices
+	p.rotation_speed = rotation_speed
+	p.center = polygon:get_centroid(p)
+	p.bounding_box = polygon:get_bounding_box(p)
+	return p
+end
+
+return polygon
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local v = require 'lib/vector'
+local polygon = require 'lib/polygon'
 require 'lib/errorexplorer'
 
 WINDOW_WIDTH = 1920
@@ -64,60 +65,9 @@ function player:shoot()
 	table.insert(bullets, bullet)
 end
 
-local pentagron = {
-	vertices = {520, 520, 480, 520, 480, 460, 800, 600},
-	rotation_speed = 0.25, -- rotation at radians per frame
-	center_x = 551,
-	center_y = 510,
-}
+local p_vert = {600, 520, 400, 520, 370, 350,  480, 300, 600, 350}
+local pentagron = polygon:create(p_vert, 0.1)
 
-function polygon_rotate(dt, polygon)
-	local angle = polygon.rotation_speed * dt
-	local s = math.sin(angle)
-	local c = math.cos(angle)
-
-	-- this is pain
-	for i = 1, #polygon.vertices, 2 do
-		local x = polygon.vertices[i] - polygon.center_x
-		local y = polygon.vertices[i+1] - polygon.center_y
-
-		local xnew = x * c - y * s
-		local ynew = x * s + y * c
-
-		x = xnew + polygon.center_x
-		y = ynew + polygon.center_y
-		polygon.vertices[i], polygon.vertices[i+1] = x, y
-	end
-end
-
-function polygon_translate(dt, polygon, vector)
-	local dx = vector[1] * dt
-	local dy = vector[2] * dt
-	for i = 1, #polygon.vertices, 2 do
-		
-		local x = polygon.vertices[i]
-		local y = polygon.vertices[i+1]
-
-		x = x + dx
-		y = y + dy
-
-		polygon.vertices[i], polygon.vertices[i+1] = x, y
-	end
-	polygon.center_x = polygon.center_x + dx
-	polygon.center_y = polygon.center_y + dy
-end
-
-function offset(vector, offset)
-	local v = {}
-	for _, value in ipairs(vector) do
-		table.insert(v, value)
-	end
-	for i = 1, #v, 2 do
-		v[i] = v[i] + offset[1]
-		v[i + 1] = v[i + 1] + offset[2]
-	end
-	return v
-end
 
 function love.update(dt)
 	fps = love.timer.getFPS()
@@ -136,8 +86,9 @@ function love.update(dt)
 		player.shot_timer = love.timer.getTime()
 	end
 
-	polygon_translate(dt, pentagron, {100, -70})
-	polygon_rotate(dt, pentagron)
+	polygon:translate(dt, pentagron, {25, -40})
+	polygon:rotate(dt, pentagron)
+	pentagron.bounding_box = polygon:get_bounding_box(pentagron)
 
 end
 
@@ -148,6 +99,7 @@ function love.draw()
 		love.graphics.circle('fill', b.coordinates[1], b.coordinates[2], b.size)
 	end
 	love.graphics.polygon('fill', pentagron.vertices)
+	love.graphics.rectangle('line', pentagron.bounding_box[1], pentagron.bounding_box[2], pentagron.bounding_box[3] - pentagron.bounding_box[1], pentagron.bounding_box[4] - pentagron.bounding_box[2])
 	love.graphics.print(love.mouse.getX(), love.mouse.getX() + 30, love.mouse.getY() + 30)
 	love.graphics.print(love.mouse.getY(), love.mouse.getX() + 60, love.mouse.getY() + 30)
 end
